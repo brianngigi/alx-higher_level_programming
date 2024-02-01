f785ee4c34c395f0a5f1f5ebfd13ea81f48c5be2
brian ngigi <briannganga6072@gmail.com>
2024-02-01 08:30:47 -0800

    TABLE

--- 0x1A-hash_tables/5-main.c
+++ 0x1A-hash_tables/5-main.c
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include "hash_tables.h"
 
-
 /**
  * main - check the code
  *
@@ -12,7 +11,6 @@
 int main(void)
 {
     hash_table_t *ht;
-
 
     ht = hash_table_create(1024);
     hash_table_print(ht);
